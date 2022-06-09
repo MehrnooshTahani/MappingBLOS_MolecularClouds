304b365548c17504bac332cace803a1282429a71
John-Ming-Ngo <john.ming.ngo@gmail.com>
2022-06-09 17:13:30 -0600

    Script 7 will no longer just crash if a given density or temperature percentage is unavailable, but will iterate through all other percentages.

--- MolecularClouds/07UncertaintyAnalysis.py
+++ MolecularClouds/07UncertaintyAnalysis.py
@@ -34,10 +34,15 @@ regionOfInterest = Region(cloudName)
 
 # -------- DEFINE FILES AND PATHS --------
 BFilePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.prefix_BLOSPointData + cloudName + '.txt')
-BData_Density50IncreasePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_densitySensitivity, 'B_Av_T0_n+50.txt')
-BData_Density50DecreasePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_densitySensitivity, 'B_Av_T0_n-50.txt')
-BData_Temp20IncreasePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_temperatureSensitivity, 'B_Av_T+20_n0.txt')
-BData_Temp20DecreasePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_temperatureSensitivity, 'B_Av_T-20_n0.txt')
+
+BData_DensityPathFragment = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_densitySensitivity)
+BData_TempPathFragment = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_temperatureSensitivity)
+
+#BData_Density50IncreasePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_densitySensitivity, 'B_Av_T0_n+50.txt')
+#BData_Density50DecreasePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_densitySensitivity, 'B_Av_T0_n-50.txt')
+#BData_Temp20IncreasePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_temperatureSensitivity, 'B_Av_T+20_n0.txt')
+#BData_Temp20DecreasePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.dir_temperatureSensitivity, 'B_Av_T-20_n0.txt')
+
 saveFilePath = os.path.join(config.dir_root, config.dir_fileOutput, cloudName, config.prefix_BLOSUncertainty + cloudName + '.txt')
 # -------- DEFINE FILES AND PATHS --------
 
@@ -67,27 +72,60 @@ TotalRMErrStDevinB = (BData['Magnetic_Field(uG)']) * (BData['TotalRMScaledErrWit
 BTotalUpperUncertainty = []
 BTotalLowerUncertainty = []
 
-try:
-    BChemDens50Increase = list(pd.read_csv(BData_Density50IncreasePath)['Magnetic_Field(uG)'])
-    BChemDens50Decrease = list(pd.read_csv(BData_Density50DecreasePath)['Magnetic_Field(uG)'])
-except:
-    print("----------------------------------------------------------------")
-    print("Error: One of the following data files could not be read!")
-    print(BData_Density50IncreasePath)
-    print(BData_Density50DecreasePath)
-    print("This script will fail, and the uncertainties cannot be calculated.")
-    print("----------------------------------------------------------------")
+BChemDensIncrease = None
+BChemDensDecrease = None
+BChemTempIncrease = None
+BChemTempDecrease = None
 
-try:
-    BChemTemp20Increase = list(pd.read_csv(BData_Temp20IncreasePath)['Magnetic_Field(uG)'])
-    BChemTemp20Decrease = list(pd.read_csv(BData_Temp20DecreasePath)['Magnetic_Field(uG)'])
-except:
-    print("----------------------------------------------------------------")
-    print("Error: One of the following data files could not be read!")
-    print(BData_Temp20IncreasePath)
-    print(BData_Temp20DecreasePath)
-    print("This script will fail, and the uncertainties cannot be calculated.")
-    print("----------------------------------------------------------------")
+DensPercent = [1, 2.5, 5, 10, 20, 30, 40, 50]
+DensPercent = DensPercent[::-1]
+TempPercent = [5, 10, 20]
+TempPercent = TempPercent[::-1]
+
+errDensPercent = []
+for densPercent in DensPercent:
+    try:
+        BData_DensityIncreasePath = os.path.join(BData_DensityPathFragment, "B_Av_T0_n+{}.txt".format(densPercent))
+        BData_DensityDecreasePath = os.path.join(BData_DensityPathFragment, "B_Av_T0_n-{}.txt".format(densPercent))
+        BChemDensIncrease = list(pd.read_csv(BData_DensityIncreasePath)['Magnetic_Field(uG)'])
+        BChemDensDecrease = list(pd.read_csv(BData_DensityDecreasePath)['Magnetic_Field(uG)'])
+        break
+    except:
+        errDensPercent.append(densPercent)
+
+if len(errDensPercent) > 0:
+    print('-------------------------------------------------------------------------------')
+    print('Warning: The following density changes percentages were not used to calculate the uncertainty due to an error.')
+    print('{}'.format(errDensPercent))
+    print('Please review the results.')
+    print('-------------------------------------------------------------------------------')
+
+errTempPercent = []
+for tempPercent in TempPercent:
+    try:
+        BData_TempIncreasePath = os.path.join(BData_TempPathFragment, "B_Av_T+{}_n0.txt".format(tempPercent))
+        BData_TempDecreasePath = os.path.join(BData_TempPathFragment, "B_Av_T-{}_n0.txt".format(tempPercent))
+        BChemTempIncrease = list(pd.read_csv(BData_TempIncreasePath)['Magnetic_Field(uG)'])
+        BChemTempDecrease = list(pd.read_csv(BData_TempDecreasePath)['Magnetic_Field(uG)'])
+        break
+    except:
+        errTempPercent.append(tempPercent)
+
+if len(errTempPercent) > 0:
+    print('-------------------------------------------------------------------------------')
+    print('Warning: The following temperature changes percentages were not used to calculate the uncertainty due to an error.')
+    print('{}'.format(errTempPercent))
+    print('Please review the results.')
+    print('-------------------------------------------------------------------------------')
+
+if BChemDensDecrease is None or BChemDensIncrease is None or BChemTempDecrease is None or BChemTempIncrease is None:
+    print('-------------------------------------------------------------------------------')
+    print(
+        'Warning: There is insufficient data to calculate the uncertainty with!')
+    print('Make sure the last two scripts (5, 6) were run before this script.')
+    print('This script will fail.')
+    print('Please review the results.')
+    print('-------------------------------------------------------------------------------')
 
 for index in range(len(BData)):
     upperDeltaBExt, lowerDeltaBExt = extinctionChemUncertainties(
@@ -95,10 +133,10 @@ for index in range(len(BData)):
         BData['BField_of_Max_Extinction'][index])
 
     upperDeltaBChemDens, lowerDeltaBChemDens = extinctionChemUncertainties(
-        BData['Magnetic_Field(uG)'][index], BChemDens50Increase[index], BChemDens50Decrease[index])
+        BData['Magnetic_Field(uG)'][index], BChemDensIncrease[index], BChemDensDecrease[index])
 
     upperDeltaBChemTemp, lowerDeltaBChemTemp = extinctionChemUncertainties(
-        BData['Magnetic_Field(uG)'][index], BChemTemp20Increase[index], BChemTemp20Decrease[index])
+        BData['Magnetic_Field(uG)'][index], BChemTempIncrease[index], BChemTempDecrease[index])
 
     # Calculate uncertainties
     BTotalUpperUncertainty.append("{0:.0f}".format(round(((TotalRMErrStDevinB[index]) ** 2 + upperDeltaBExt ** 2
@@ -115,5 +153,6 @@ FinalBLOSResults['TotalLowerBUncertainty'] = BTotalLowerUncertainty
 
 # -------- SAVE FINAL BLOS RESULTS --------
 FinalBLOSResults.to_csv(saveFilePath, index=False)
+
 print('Saving calculated magnetic field values and associated uncertainties to '+saveFilePath)
 # -------- SAVE FINAL BLOS RESULTS --------
